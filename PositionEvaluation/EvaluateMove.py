6cb0793b90e44cd88d8e0101fc570c795300641d
Nathanael <njluke@cedarville.edu>
2025-04-03 22:33:45 -0400

    Fixed some stuff. Changed some stuff. Hopefully, didn't break anything.

--- PositionEvaluation/EvaluateMove.py
+++
@@ -1,9 +0,0 @@
-import chess
-import pygame
-
-#@title Setup board and print legal moves
-board = chess.Board()
-print(board)
-legMov = list(board.legal_moves)
-board = chess.Board()
-print(f"There are {len(legMov)} legal moves")
